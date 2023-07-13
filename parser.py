5eedf10967c41eac524f4253faf37c2d88ea671f
shaojiajun314 <63770117@qq.com>
2023-07-14 02:26:58 +0800

    init

---
+++ parser.py
@@ -0,0 +1,51 @@
+import re
+import sys
+from urllib.parse import unquote
+from urllib.parse import parse_qsl
+
+
+def url_parse(name):
+    pattern = re.compile(
+        r"""
+            (?P<name>[\w\+]+)://
+            (?:
+                (?P<username>[^:/]*)
+                (?::(?P<password>[^@]*))?
+            @)?
+            (?:
+                (?:
+                    \[(?P<ipv6host>[^/\?]+)\] |
+                    (?P<ipv4host>[^/:\?]+)
+                )?
+                (?::(?P<port>[^/\?]*))?
+            )?
+            (?:/(?P<database>[^\?]*))?
+            (?:\?(?P<query>.*))?
+            """,
+        re.X,
+    )
+    m = pattern.match(name)
+    if m is not None:
+        components = m.groupdict()
+        if components["query"] is not None:
+            query = {}
+            for key, value in parse_qsl(components["query"]):
+                if key in query:
+                    query[key] = list(query[key]) if isinstance(query[key], list) else [query[key]]
+                    query[key].append(value)
+                else:
+                    query[key] = value
+        else:
+            query = None
+        components["query"] = query
+        if components["username"] is not None:
+            components["username"] = unquote(components["username"])
+        if components["password"] is not None:
+            components["password"] = unquote(components["password"])
+        ipv4host = components.pop("ipv4host")
+        ipv6host = components.pop("ipv6host")
+        components["host"] = ipv4host or ipv6host
+        name = components.pop("name")
+        if components["port"]:
+            components["port"] = int(components["port"])
+        return name, components
